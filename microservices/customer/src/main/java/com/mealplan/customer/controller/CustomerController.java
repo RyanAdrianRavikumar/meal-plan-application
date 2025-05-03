249de3e6f63f7289cb6652cde59c676e0f773f55
Ryan <ryanadrianravikumar@gmail.com>
2025-05-03 16:28:07 +0100

    add update and delete endpoints to customer

--- microservices/customer/src/main/java/com/mealplan/customer/controller/CustomerController.java
+++ microservices/customer/src/main/java/com/mealplan/customer/controller/CustomerController.java
@@ -3,9 +3,7 @@ package com.mealplan.customer.controller;
 import com.mealplan.customer.entity.Customer;
 import com.mealplan.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
@@ -16,5 +14,15 @@ public class CustomerController {
     @GetMapping(path = "/customers/{id}")
     public Customer getCustomerById(@PathVariable int id){
         return customerService.getCustomerById(id);
+    }
+
+    @PutMapping(path = "/customers/{id}")
+    public String updateCustomerById(@PathVariable int id, @RequestBody Customer updatedCustomer){
+        return customerService.updateCustomerById(id, updatedCustomer);
+    }
+
+    @DeleteMapping(path = "/customers/{id}")
+    public String deleteCustomerById(@PathVariable int id){
+        return customerService.deleteCustomerById(id);
     }
 }
--- microservices/customer/src/main/java/com/mealplan/customer/service/CustomerService.java
+++ microservices/customer/src/main/java/com/mealplan/customer/service/CustomerService.java
@@ -20,4 +20,29 @@ public class CustomerService {
         }
         return null;
     }
+
+    public String updateCustomerById(int id, Customer updatedCustomer){
+        Optional<Customer> validCustomer = customerRepository.findById(id);
+        if(validCustomer.isPresent()){
+            Customer customer = validCustomer.get();
+            customer.setCustomerName(updatedCustomer.getCustomerName());
+            customer.setCustomerEmail(updatedCustomer.getCustomerEmail());
+            customer.setCustomerPhone(updatedCustomer.getCustomerPhone());
+            customer.setCustomerPassword(updatedCustomer.getCustomerPassword());
+
+            customerRepository.save(customer);
+            return "Customer updated successfully.";
+        } else {
+            return "Customer not found.";
+        }
+    }
+
+    public String deleteCustomerById(int id){
+        Optional<Customer> customer = customerRepository.findById(id);
+        if(customer.isPresent()){
+            customerRepository.deleteById(id);
+            return "Customer deleted successfully.";
+        }
+        return "Customer not found.";
+    }
 }
